2e3a3ae3dfdd59b21cead34d64416bbc61dda2e2
noresources <dennis.petta@gmail.com>
2019-10-18 17:32:56 +0300

    added about file

---
+++ __about__.py
@@ -0,0 +1,8 @@
+__title__ = 'contactTree-api'
+__package_name = 'contactTree-api'
+__version__ = '0.1.0'
+__description__ = ''
+__author__ = 'Dionisis Pettas'
+__email__ = ''
+__github__  = 'https://github.com/deepettas/contact-tree'
+__licence__ = 'MIT'
